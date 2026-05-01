668ce427bca34fb602bb82f72bf05997b9fccc3d
macsobel <macsobel@gmail.com>
2026-04-30 21:12:57 -0400

    style: update VPS sync button icon to circular refresh

--- frontend/pages/settings.js
+++ frontend/pages/settings.js
@@ -45,7 +45,7 @@ const SettingsPage = {
                         Rescan Tables
                     </button>
                     <button class="btn btn-secondary" id="btn-sync-vps">
-                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3"/></svg>
+                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8"/><path d="M21 3v5h-5"/><path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16"/><path d="M3 21v-5h5"/></svg>
                         Sync VPS Database
                     </button>
                     <button class="btn btn-secondary" id="btn-media-preferences" onclick="window.location.hash = 'media-preferences'">
